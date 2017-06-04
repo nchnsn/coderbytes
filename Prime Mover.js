2131da5a3088927144b4833815b8e5eb78d7971f
nchnsn <nchnsn@gmail.com>
2017-06-04 14:07:09 -0700

    Finished medium coder bytes problem 'Prime Mover'

---
+++ Prime Mover.js
@@ -0,0 +1,26 @@
+// Prime Mover
+
+/*Have the function PrimeMover(num) return the numth prime number. The range will be from 1 to 10^4. For example: if num is 16 the output should be 53 as 53 is the 16th prime number. */
+/**/
+/*Use the Parameter Testing feature in the box below to test your code with different arguments.*/
+
+function PrimeMover(num) {
+    var numOfPrimes = 0;
+    var currentPrime = null;
+    for(var i = 2; num > numOfPrimes; i++){
+        if (isPrime(i)){
+            numOfPrimes++; 
+            currentPrime = i;
+        }
+    }
+    return currentPrime;     
+}
+
+function isPrime(num){
+   for(var i = num - 1; i > 1; i--){
+      if(num % i === 0){
+          return false;
+      }
+   }
+   return true;
+}
